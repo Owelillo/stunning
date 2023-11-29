aef18f9612a865bc615697a1478f16a5122e8d22
Gabriel <owelcontacto@gmail.com>
2023-11-29 11:02:17 +0100

    Ahora indico mi localidad de residencia

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
+    System.out.println("Gabriel Rojas Barredo");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Contenido 4a");
+    System.out.println("IES San Andrés");
+    System.out.println("Mansilla de las Mulas");
   }
   
 }
